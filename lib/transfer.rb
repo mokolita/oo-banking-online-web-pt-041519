ed891841294d2e53851a79cbc6216eb1f41a13d6
Meg Galvez <meg@oakmaine.com>
2019-05-21 16:24:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,6 +27,10 @@ class Transfer
   end 
   
   def reverse_transfer
-    
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end 
  
 end
