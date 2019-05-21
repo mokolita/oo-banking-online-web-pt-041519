a41df9b438348ec6309d6e6ab59cd536786912ed
Meg Galvez <meg@oakmaine.com>
2019-05-21 16:21:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
  
   def execute_transaction
-    if sender.balance > @amount && 
+    if sender.balance > @amount && @status == "pending"
     @sender.balance -= @amount 
     @receiver.balance += @amount
   
